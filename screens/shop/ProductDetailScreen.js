491df5240d7a9a3a1261996578a5351d9dd90b78
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-04-12 06:17:12 +0700

    ^ product detail style

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -7,26 +7,59 @@ import {
     ScrollView,
     StyleSheet
 } from 'react-native';
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
+import Color from '../../constants/Color';
 
 const ProductDetailScreen = (props) => {
-    const productId = props.navigation.getParam('productId')
-    const selectedProduct = useSelector(state => state.products.availableProducts.find(prod => prod.id === productId))
-    return (
-        <View>
-            <Text>{selectedProduct.title}</Text>
-        </View>
+    const productId = props.navigation.getParam('productId');
+    const selectedProduct = useSelector((state) =>
+        state.products.availableProducts.find((prod) => prod.id === productId)
     );
-}
+    return (
+        <ScrollView>
+            <Image
+                style={styles.image}
+                source={{ uri: selectedProduct.imageUrl }}
+            />
+            <View style={styles.action}>
+                <Button color={Color.primary} title="Add to Cart" onPress={() => {}} />
+            </View>
+            <Text style={styles.price}>
+                ${selectedProduct.price.toFixed(2)}
+            </Text>
+            <Text style={styles.description}>
+                {selectedProduct.description}
+            </Text>
+        </ScrollView>
+    );
+};
 
-ProductDetailScreen.navigationOptions = navData => {
+ProductDetailScreen.navigationOptions = (navData) => {
     return {
         headerTitle: navData.navigation.getParam('title')
+    };
+};
+
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 300
+    },
+    action: {
+        marginVertical: 10,
+        alignItems: 'center'
+    },
+    price: {
+        fontSize: 20,
+        color: '#888',
+        textAlign: 'center',
+        marginVertical: 20
+    },
+    description: {
+        fontSize: 14,
+        textAlign: 'center',
+        marginHorizontal: 20
     }
-}
-
-const style = StyleSheet.create({
-
-})
+});
 
 export default ProductDetailScreen;
