4f470cd879eea57945e00833612fda4b3d132009
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-05-17 18:03:22 +0700

    ^ fetch stored orders

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { FlatList, Platform, Text } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { FlatList, Platform, ActivityIndicator, View, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../../components/UI/HeaderButton';
 import OrderItem from '../../components/shop/OrderItem';
+import * as orderActions from '../../store/actions/orders'
+import Color from '../../constants/Color'
 
 const OrdersScreen = (props) => {
+    const [isLoading, setIsLoading] = useState(false)
     const orders = useSelector((state) => state.orders.orders);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        setIsLoading(true)
+        dispatch(orderActions.fetchOrder()).then(() => setIsLoading(false));
+    }, [dispatch])
+
+    if(isLoading) {
+        return <View style={styles.centered}>
+            <ActivityIndicator size="large" color={Color.primary} />
+        </View>
+    }
 
     return (
         <FlatList
@@ -41,5 +56,13 @@ OrdersScreen.navigationOptions = (navData) => {
         )
     };
 };
+
+const styles = StyleSheet.create({
+    centered: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
 
 export default OrdersScreen;
--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,4 +1,37 @@
+import Order from '../../models/order'
 export const ADD_ORDER = 'ADD_ORDER';
+export const SET_ORDER = 'SET_ORDER';
+
+export const fetchOrder = () => {
+    return async dispatch => {
+        try {
+            const response = await fetch(
+                'https://rn-shop-app-c81ba.firebaseio.com/orders/u1.json'
+            );
+            console.log(response)
+
+            if(!response.ok) {
+                throw new Error('Something went wrong!')
+            }
+    
+            const resData = await response.json();
+            const loadedOrders = [];
+            for (const key in resData) {
+                loadedOrders.push(
+                    new Order(
+                        key,
+                        resData[key].cartItems,
+                        resData[key].totalAmount,
+                        new Date(resData[key].date),
+                    )
+                );
+            }
+            dispatch({type: SET_ORDER, orders: loadedOrders})
+        }catch(e) {
+            throw new Error('Something went wrong!')
+        }
+    }
+}
 
 export const addOrder = (cartItems, totalAmount) => {
     return async (dispatch) => {
